e9434f4f92517457dc3b7cb81dfd84e93b260e8e
Juan David <juandavidarguello@gmail.com>
2020-11-06 00:40:35 +0100

    CF

--- Simulación/App/CFD_DEM/.ipynb_checkpoints/Geom-checkpoint.py
+++ Simulación/App/CFD_DEM/.ipynb_checkpoints/Geom-checkpoint.py
@@ -1,6 +1,5 @@
 import pyvista as pv
 import pygmsh
-import os
 import ipywidgets as widgets
 from math import pi, radians, cos, sin, tan
 import meshio
@@ -62,22 +61,33 @@ class Geometry():
 
             flat = geom.boolean_union([entrada, lodos])
 
-            geom.extrude(flat, [0, 0, e])
+            top, volume, lat = geom.extrude(flat, [0, 0, e])
+            
+            #Nombres de las Fronteras
+            geom.add_physical(flat, label="bottom") 
+            geom.add_physical(top, label="top") 
+            #geom.add_physical(volume, label="volume") 
+            #geom.add_physical(lat, label="lat")
+            print(volume.__attr__)
             
             mesh = geom.generate_mesh()
             
-            mesh.write( os.getcwd() + "/" + name)
+            mesh.write(name)
             nombre = name.replace(".vtk", ".msh")
-            mesh.write(os.getcwd() + "/" + nombre, file_format="gmsh22")
+            mesh.write(nombre, file_format="gmsh22")
     
     def guardar(self, file="out.msh"):
+        saved_file = file
+        dolfin = pv.read(saved_file)
+        qual = dolfin.compute_cell_quality()
+        qual.plot(show_edges=True)
+    
+    def convertir(self, file="out"):
         saved_file = os.getcwd() + '/' + file
         dolfin = pv.read(saved_file)
         qual = dolfin.compute_cell_quality()
         #self.convertir()
         qual.plot(show_edges=True)
-    
-    def convertir(self, file="out"):
         root =  os.getcwd() + '/'
         read = meshio.read(
             root + file + '.vtk'
