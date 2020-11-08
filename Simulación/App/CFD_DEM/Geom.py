078d7efe0b8e3112fe117664e413236ff82a9bee
Juan <juandavidarguello@gmail.com>
2020-11-08 10:50:44 -0500

    .

--- Simulación/App/CFD_DEM/Geom.py
+++ Simulación/App/CFD_DEM/Geom.py
@@ -67,13 +67,13 @@ class Geometry():
             geom.add_physical(flat, label="bottom") 
             geom.add_physical(top, label="top") 
             #geom.add_physical(volume, label="volume") 
-            #geom.add_physical(lat, label="lat")
+            geom.add_physical(lat[0], label="lat")
             
             mesh = geom.generate_mesh()
             
             mesh.write(name)
-            nombre = name.replace(".vtk", ".msh")
-            mesh.write(nombre, file_format="gmsh22")
+            nombre = name.replace(".vtk", ".stl")
+            mesh.write(nombre, file_format="stl", binary=False)
     
     def guardar(self, file="out.msh"):
         saved_file = file
